0a79ada14ec5706a39c531f18ca50f25462dcb98
Bennett707 <rogerbennett382@gmail.com>
2022-09-09 12:02:47 -0500

    Update

--- Code/Programmingtest.java
+++ Code/Programmingtest.java
@@ -1,14 +1,15 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Programmingtest {
 	public static void main(String[] args) {
-		Scanner scnr = new Scanner(System.in);
-		int x;
-		int y;
-		
-		System.out.print("Enter an Interger: ");
-		x = scnr.nextInt();
-		y = x * 5;
-		System.out.println(y);
+		try (Scanner scnr = new Scanner(System.in)) {
+			int x;
+			int y;
+			
+			System.out.print("Enter an Interger: ");
+			x = scnr.nextInt();
+			y = x * 5;
+			System.out.println(y);
+		}
 	}
 }
